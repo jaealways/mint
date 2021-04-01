908968c7e8938dc0a0dca974937b45af64333fb8
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-04-01 10:40:07 +0900

    Update collector_sql.py

--- collector_sql.py
+++ collector_sql.py
@@ -1,10 +1,33 @@
-from sqlalchemy import Interger, Text
+from sqlalchemy import create_engine
+import pymysql
+print("collector_sql 시작")
 
-class collector_sql:
+
+class Collector:
     def __init__(self):
+        self.engine_bot = None
 
+    def db_setting(self, db_name, db_id, db_passwd, db_ip, db_port):
+        self.engine_bot = create_engine("mysql+mysqldb://" + db_id + ":" + db_passwd + "@"
+                                        + db_ip + ":" + db_port + "/" + db_name, encoding = 'utf-8')
 
         self.collector
 
-    def sql_collecting(self):
-        sql = "select "
+    # def sql_collecting(self):
+    #     sql = "select "
+
+print("collector_sql.py의 __name__:", __name__)
+
+if __name__ == "__main__":
+    print("__main__에 들어옴")
+    c = Collector()
+    db_name = 'music_cow'
+    db_id = 'kuggle2'
+    db_ip = ''
+    db_passwd = 'abc123456789'
+    db_port = '3306'
+
+    sql = "select * from bot_test1.class1;"
+
+    rows = c.engine_bot.execute(sql).fetchall()
+    print(rows)
