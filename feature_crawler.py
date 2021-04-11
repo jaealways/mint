5bc7eb721385db520f6b904fd327a49cf29f4cd1
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-04-11 14:19:58 +0900

    Update feature_crawler.py

--- feature_crawler.py
+++ feature_crawler.py
@@ -36,52 +36,52 @@ song_artist = "거미"
 
 # 관련성 기준으로 검색
 
-# page_Youtube = "https://www.youtube.com/search?q={0}&sp=EgIQAQ%253D%253D".format(song_artist + ' ' + song_title)
-#
-# driver = webdriver.Chrome()
-# driver.get(page_Youtube)
-#
-# html_Youtube = driver.page_source
-# soup_Youtube = BeautifulSoup(html_Youtube, 'html.parser')
-#
-# search_num_Youtube = soup_Youtube.select('div.title-wrapper')
-#
-# for n in search_num_Youtube:
-#     search_Youtube = n.text.strip()
-#     print(search_Youtube)
-
-#driver.close()
-
-
 page_Youtube = "https://www.youtube.com/search?q={0}&sp=EgIQAQ%253D%253D".format(song_artist + ' ' + song_title)
 
+driver = webdriver.Chrome()
+driver.get(page_Youtube)
 
-search_Youtube_info = {
-    'title':'',
-    'video_link':'',
-    'hits' : '',
-    'updated_time':''
-}
+html_Youtube = driver.page_source
+soup_Youtube = BeautifulSoup(html_Youtube, 'html.parser')
 
-def search_Youtube(page_Youtube):
-    response = requests.get(page_Youtube)
-    soup = BeautifulSoup(response.text, "lxml")
-    lis = soup.find_all('meta', {'class' : 'style-scope ytd-video-render'})
-    for li in lis :
-        title = li.find('a', {'title' : True})['title']
-        video_link = 'https://www.youtube.com' + li.find('a', {'href' : True})['href']
-        hits = li.find_all('li')[2].text
-        updated_time = li.find_all('li')[3].text
-        search_Youtube_info = {
-            'title' : title,
-            'video_link' : video_link,
-            'hits' : hits,
-            'updated_time' : updated_time
-        }
-        print(search_Youtube_info)
-    return search_Youtube_info
+search_num_Youtube = soup_Youtube.select('div.title-wrapper')
 
-search_Youtube(page_Youtube)
+for n in search_num_Youtube:
+    search_Youtube = n.text.strip()
+    print(search_Youtube)
+
+# driver.close()
+
+
+# page_Youtube = "https://www.youtube.com/search?q={0}&sp=EgIQAQ%253D%253D".format(song_artist + ' ' + song_title)
+#
+#
+# search_Youtube_info = {
+#     'title':'',
+#     'video_link':'',
+#     'hits' : '',
+#     'updated_time':''
+# }
+#
+# def search_Youtube(page_Youtube):
+#     response = requests.get(page_Youtube)
+#     soup = BeautifulSoup(response.text, "lxml")
+#     lis = soup.find_all('meta', {'class' : 'style-scope ytd-video-render'})
+#     for li in lis :
+#         title = li.find('a', {'title' : True})['title']
+#         video_link = 'https://www.youtube.com' + li.find('a', {'href' : True})['href']
+#         hits = li.find_all('li')[2].text
+#         updated_time = li.find_all('li')[3].text
+#         search_Youtube_info = {
+#             'title' : title,
+#             'video_link' : video_link,
+#             'hits' : hits,
+#             'updated_time' : updated_time
+#         }
+#         print(search_Youtube_info)
+#     return search_Youtube_info
+#
+# search_Youtube(page_Youtube)
 
 
 
