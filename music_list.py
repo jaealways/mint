9c890cf0669513308d2f6ed52f5fc0e3fd874397
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-04-30 15:55:33 +0900

    20210430 1554
    To intergrate crawler as class html_crawler (fixing)

--- music_list.py
+++ music_list.py
@@ -7,17 +7,21 @@ import pandas as pd
 
 class MusicList():
     def __init__(self):
-
+        # 뮤직카우 크롤링 시작
         self.crawl_link()
-
+        # 아티스트랑 타이틀 분류하기(feat, prod)
         self.name_classifier()
-
+        # DB에 저장하기
         self.collect_db()
 
 
     def crawl_link(self):
-        print("뮤직카우  크롤링 시작")
-        self.
+        print("뮤직카우 크롤링 시작")
+        for num in range(0,2000):
+            self.page = "https://www.musicow.com/song/{0}?tab=info".format(num)
+            self.html = BeautifulSoup((requests.get(self.page)).text, 'html.parser')
+            self.
+
 
     def name_classifier
 
