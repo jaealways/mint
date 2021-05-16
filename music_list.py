313ae0619a21f941eaeab34fd9f9e53c9220f0ae
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-05-16 22:43:02 +0900

    Update music_list.py

--- music_list.py
+++ music_list.py
@@ -7,60 +7,62 @@ import time
 
 
 class MusicList():
-    def __init__(self):
+    def __init__(self, num):
         # 뮤직카우 크롤링 시작
+        self.num = num
         self.crawl_link()
+
 
 
     def crawl_link(self):
         print("뮤직카우 크롤링 시작")
-        for self.num in range(0,2000):
-            self.page = "https://www.musicow.com/song/{0}?tab=info".format(self.num)
-            self.url = requests.get(self.page)
-            self.html = self.url.text
-            self.soup = BeautifulSoup(self.html, 'html.parser')
+        self.page = "https://www.musicow.com/song/{0}?tab=info".format(self.num)
+        self.url = requests.get(self.page)
+        self.html = self.url.text
+        self.soup = BeautifulSoup(self.html, 'html.parser')
 
-            self.song_title = str(self.soup.select('div.song_header > div.information > p > strong'))
-            self.song_title = re.sub('<.+?>', '', self.song_title, 0).strip().replace('[', '').replace(']','')
+        self.song_title = str(self.soup.select('div.song_header > div.information > p > strong'))
+        self.song_title = re.sub('<.+?>', '', self.song_title, 0).strip().replace('[', '').replace(']','')
 
-            print("{0}번째 노래 저장 중".format(self.num))
+        print("{0}번째 노래 저장 중".format(self.num))
 
-            #노래 제목이 없을 경우 비어있는 페이지이므로 pass
-            if self.song_title[1:-1]=='':
-                pass
-            else:
+        #노래 제목이 없을 경우 비어있는 페이지이므로 pass
+        if self.song_title[1:-1]=='':
+            pass
+        else:
+            self.song_artist = str(self.soup.select('div.song_header > div.information > em'))
+            self.song_artist = re.sub('\<.+?>|\[|\'|\]', '', self.song_artist, 0).strip()
 
-                #위에서 pass되지 않은 곡만 기록
-                #re.sub -> 정규표현식
-                self.song_artist = str(self.soup.select('div.song_header > div.information > em'))
-                self.song_artist = re.sub('<.+?>', '', self.song_artist, 0).strip().replace('[', '').replace(']','')
+            self.auc_date_1 = str(self.soup.select('div:nth-child(1) > h2 > small'))
+            self.auc_date_1 = re.sub('<.+?>', '', self.auc_date_1, 0).strip().replace('[', '').replace(']','')
+            self.auc_stock_1 = str(self.soup.select('div.card_body > div > div:nth-child(1) > dl > dd:nth-child(2)'))
+            self.auc_stock_1 = re.sub('<.+?>', '', self.auc_stock_1, 0).strip().replace('[', '').replace(']','')
+            self.auc_price_1 = str(self.soup.select('div.card_body > div > div:nth-child(1) > dl > dd:nth-child(8)'))
+            self.auc_price_1 = re.sub('<.+?>', '', self.auc_price_1, 0).strip().replace('[', '').replace(']','')
 
-                self.auc_date_1 = str(self.soup.select('div:nth-child(1) > h2 > small'))
-                self.auc_date_1 = re.sub('<.+?>', '', self.auc_date_1, 0).strip().replace('[', '').replace(']','')
-                self.auc_stock_1 = str(self.soup.select('div.card_body > div > div:nth-child(1) > dl > dd:nth-child(2)'))
-                self.auc_stock_1 = re.sub('<.+?>', '', self.auc_stock_1, 0).strip().replace('[', '').replace(']','')
-                self.auc_price_1 = str(self.soup.select('div.card_body > div > div:nth-child(1) > dl > dd:nth-child(8)'))
-                self.auc_price_1 = re.sub('<.+?>', '', self.auc_price_1, 0).strip().replace('[', '').replace(']','')
+            self.auc_date_2 = str(self.soup.select('div:nth-child(2) > h2 > small'))
+            self.auc_date_2 = re.sub('<.+?>', '', self.auc_date_2, 0).strip().replace('[', '').replace(']','')
+            self.auc_stock_2 = str(self.soup.select('div.card_body > div > div:nth-child(2) > dl > dd:nth-child(2)'))
+            self.auc_stock_2 = re.sub('<.+?>', '', self.auc_stock_2, 0).strip().replace('[', '').replace(']','')
+            self.auc_price_2 = str(self.soup.select('div.card_body > div > div:nth-child(2) > dl > dd:nth-child(8)'))
+            self.auc_price_2 = re.sub('<.+?>', '', self.auc_price_2, 0).strip().replace('[', '').replace(']','')
 
-                self.auc_date_2 = str(self.soup.select('div:nth-child(2) > h2 > small'))
-                self.auc_date_2 = re.sub('<.+?>', '', self.auc_date_2, 0).strip().replace('[', '').replace(']','')
-                self.auc_stock_2 = str(self.soup.select('div.card_body > div > div:nth-child(2) > dl > dd:nth-child(2)'))
-                self.auc_stock_2 = re.sub('<.+?>', '', self.auc_stock_2, 0).strip().replace('[', '').replace(']','')
-                self.auc_price_2 = str(self.soup.select('div.card_body > div > div:nth-child(2) > dl > dd:nth-child(8)'))
-                self.auc_price_2 = re.sub('<.+?>', '', self.auc_price_2, 0).strip().replace('[', '').replace(']','')
+            self.song_date = str(self.soup.select('div.card_body > div > dl > dd:nth-child(2)'))
+            self.song_date = re.sub('<.+?>', '', self.song_date, 0).strip().replace('[', '').replace(']','')
 
-                self.song_date = str(self.soup.select('div.card_body > div > dl > dd:nth-child(2)'))
-                self.song_date = re.sub('<.+?>', '', self.song_date, 0).strip().replace('[', '').replace(']','')
+            #stock_num 앞뒤 공백 제거 후 숫자만 추출(어떻게 간추릴 수 있을지 고민 중)
+            self.stock_num = str(self.soup.select('div.card_body > div > dl > dd:nth-child(20) > p:nth-child(1)'))
+            self.stock_num = re.sub('<.+?>', '', self.stock_num, 0).strip()
+            self.stock_num = self.stock_num.replace('\t','').replace('\n','').replace('1/','').replace(',','').replace('[', '').replace(']','')
 
-                #stock_num 앞뒤 공백 제거 후 숫자만 추출(어떻게 간추릴 수 있을지 고민 중)
-                self.stock_num = str(self.soup.select('div.card_body > div > dl > dd:nth-child(20) > p:nth-child(1)'))
-                self.stock_num = re.sub('<.+?>', '', self.stock_num, 0).strip()
-                self.stock_num = self.stock_num.replace('\t','').replace('\n','').replace('1/','').replace(',','').replace('[', '').replace(']','')
 
-                # self.translating_naver()
-                # self.classify_name()
-                self.listing_youtube()
-                self.collect_db()
+            self.translating_naver()
+            # self.classify_name()
+            self.listing_youtube()
+            self.collect_db()
+
+
+
 
 
     def translating_naver(self):
@@ -77,11 +79,14 @@ class MusicList():
         self.song_artist = str(self.soup.select('div:nth-child(1) > div.info_area > div.artist > span:nth-child(1) > span > a > span'))
         self.song_artist = re.sub('<.+?>', '', self.song_artist, 0).strip().replace('[', '').replace(']','')
 
+        self.pair = self.song_artist + ' ' + self.song_title
+        self.pair = self.pair.replace('[', '').replace(']','')
 
-    def classify_name(self):
-        # Feat 분류
-        if 'Feat.' in self.song_title:
-            pos_1 = re.search('Feat.', self.song_title)
+    #
+    # def classify_name(self):
+    #     # Feat 분류
+    #     if 'Feat.' in self.song_title:
+    #         pos_1 = re.search('Feat.', self.song_title)
 
 
 
@@ -91,7 +96,7 @@ class MusicList():
 
 
         # 원제 한글 영어
-        self.song
+        # self.song
 
 
     def listing_youtube(self):
@@ -106,6 +111,7 @@ class MusicList():
         search_num_Youtube = soup_Youtube.select('a#video-title')
         count = 0
         self.youtube_video = []
+        print("Youtube List 출력 중")
 
         for i in search_num_Youtube:
             count+=1
@@ -122,7 +128,6 @@ class MusicList():
     def collect_db(self):
         client = MongoClient('localhost', 27017)
         db = client.music_cow
-        db.myCol
         posts = db.posts
 
         print("{0}번째 노래 DB 입력 중".format(self.num))
@@ -147,8 +152,18 @@ class MusicList():
 
 #for 문 클래스 밖으로 빼기??
 
+
+
 if __name__ == '__main__':
     driver = webdriver.Chrome()
-    MusicList()
+    for num in range(26,2000):
+        MusicList(num)
+
 
 driver.close()
+
+
+
+# 1. 수작업 엑셀
+# 2. 트위터 바이바이(?)
+# 3.
