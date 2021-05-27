19eed467cd1eec85663412a95bfa39a1dc755cc4
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-05-27 18:09:42 +0900

    Update daily_crawler_youtube.py

--- daily_crawler_youtube.py
+++ daily_crawler_youtube.py
@@ -1,8 +1,5 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
+from googleapiclient.discovery import build
 from pymongo import MongoClient
-import re
-import time
 
 class YoutubeDailyCrawler:
     def __init__(self):
@@ -11,78 +8,66 @@ class YoutubeDailyCrawler:
     def read_db(self):
         list_db_music = col1.find({}, {'num': {"$slice": [1, 1]}})
         for x in list_db_music:
-            for num_video_order in range(1,11):
+            for num_video_order in range(1, 11):
                 self.link_video = x['video{0}'.format(num_video_order)]['link']
+                self.id_video = self.link_video.split('watch?v=')[1]
                 self.title_video = x['video{0}'.format(num_video_order)]['title']
+                self.song_artist = x['song_artist']
+                self.song_title = x['song_title']
 
                 self.crawling_daily()
 
     def crawling_daily(self):
-        driver.get(self.link_video)
+        f = open("key.txt", 'r')
+        api_key = f.read()
+        video_id = self.id_video
 
-        html_youtube = driver.page_source
-        soup_youtube = BeautifulSoup(html_youtube, 'html.parser')
-        time.sleep(5)
-
-        search_num_youtube = soup_youtube.select('#menu > ytd-menu-renderer')
-        # > yt-formatted-string
-
-        for num_like in search_num_youtube:
-            num_like = num_like.attrs['aria-label']
-            if '좋아요' in num_like:
-                self.like = num_like
-            else:
-                self.dislike = num_like
-        count = 1
-        print("{0} Youtube 댓글 출력 중".format(self.title_video))
-
-        self.video = {
-            'title': self.title_video,
-            'like': self.like,
-            'dislike': self.dislike,
-            'num_comment': self.num_comment
-        }
-
-        for i in search_num_youtube:
-            self.comment = str(i('#content-text'))
-            self.comment = re.sub('\<.+?>|\[|\'|\]', '', self.comment, 0).strip()
-            self.comment_like = str(i('#vote-count-left'))
-            self.comment_like = re.sub('\<.+?>|\[|\'|\]', '', self.comment_like, 0).strip()
-            print(self.comment, self.comment_like)
-            time.sleep(0.2)
-
-            comment = {
-                'title': self.comment, 'like': self.comment_like
-            }
-
-            self.list_video['video{0}'.format(count)] = comment
-
-
-        print("{0} 비디오 댓글 DB 입력 중".format(self.title_video))
-
-        col2.insert_one(self.list_video).inserted_id
-
-
-    def collect_db(self):
-        daily_youtube = {
+        api_obj = build('youtube', 'v3', developerKey=api_key)
+        response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, maxResults=100).execute()
+        list_comment = {
+            'title_video': self.title_video,
+            'id_video': self.id_video,
             'song_title': self.song_title,
-            'song_artist': self.song_artist,
-
-
+            'song_artist': self.song_artist
         }
+
+
+        while response:
+            count = 1
+            for item in response['items']:
+                comment = item['snippet']['topLevelComment']['snippet']
+                comments = {
+                    'comment': comment['textDisplay'],
+                    'author': comment['authorDisplayName'],
+                    'date': comment['publishedAt'],
+                    'like': comment['likeCount']
+                }
+
+                if item['snippet']['totalReplyCount'] > 0:
+                    for reply_item in item['replies']['comments']:
+                        reply = reply_item['snippet']
+                        comments = {
+                            'comment': reply['textDisplay'],
+                            'author': comment['authorDisplayName'],
+                            'date': comment['publishedAt'],
+                            'like': comment['likeCount']
+                        }
+
+                list_comment['comment{0}'.format(count)] = comments
+                print(comments)
+                count += 1
+
+            if 'nextPageToken' in response:
+                response = api_obj.commentThreads().list(part='snippet,replies', videoId=video_id, pageToken=response['nextPageToken'], maxResults=100).execute()
+            else:
+                break
+        col2.insert_one(list_comment).inserted_id
+
 
 if __name__ == '__main__':
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--incognito')
-    driver = webdriver.Chrome(options=chrome_options)
-
     client = MongoClient('localhost', 27017)
     db = client.music_cow
     col1 = db.youtube_list
     col2 = db.daily_youtube
 
     YoutubeDailyCrawler()
-
-    driver.close()
-
-
