8a4226415e7d8a54ab51bee9baf9b1ef69bb8984
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-06-03 12:03:40 +0900

    Update song_split.py

--- song_split.py
+++ song_split.py
@@ -8,26 +8,32 @@ class SongSpliter:
     def read_db(self):
         list_db_music = col1.find({}, {'num': {"$slice": [1, 1]}})
         for x in list_db_music:
-            self.music_list = x
-            self.list_split = {}
-            self.song_artist = x['song_artist']
-            self.song_artist = self.song_artist.replace('(', ', (').split(',')
-            self.song_title = x['song_title']
-            self.song_title = self.song_title.replace('(', ', (').split(',')
+            self.num = x['num']
+            if self.num < 58:
+                pass
+            else:
+                self.num_feat_kor, self.num_feat_eng, self.num_main_kor, \
+                self.num_main_eng, self.num_sub_kor, self.num_sub_eng = 0, 0, 0, 0, 0, 0
+                self.music_list = x
+                self.list_split = {}
+                self.song_artist = x['song_artist']
+                self.song_artist = self.song_artist.replace('(', ', (').split(',')
+                self.song_title = x['song_title']
+                self.song_title = self.song_title.replace('(', ', (').split(',')
 
-            self.spliting_song()
-            self.collect_db()
+                self.spliting_song()
+                self.collect_db()
 
     def spliting_song(self):
         for title in self.song_title:
             if '(' in title:
                 song_title_sub = re.sub('|\(|\)', '', title).strip()
                 if song_title_sub[0].encode().isalpha():
-                    if 'Feat.' or 'feat.' in song_title_sub:
-                        self.song_artist_sub_feat = song_title_sub.replace('Feat.' or 'feat.', '').strip()
+                    if ('Feat.' or 'feat.') in song_title_sub:
+                        self.song_artist_sub_feat = song_title_sub.replace(('Feat.' or 'feat.'), '').strip()
                         self.feat_split()
-                    elif 'Prod.' or 'prod.' or 'PROD.' in song_title_sub:
-                        self.song_artist_sub_feat = song_title_sub.replace('Prod.' or 'prod.' or 'PROD.', '').strip()
+                    elif ('Prod.' or 'prod.' or 'PROD.') in song_title_sub:
+                        self.song_artist_sub_feat = song_title_sub.replace(('Prod.' or 'prod.' or 'PROD.'), '').replace('by', '').strip()
                         self.feat_split()
                     else:
                         self.list_split['song_title_sub_eng'] = song_title_sub.strip()
@@ -44,21 +50,29 @@ class SongSpliter:
             if '(' in artist:
                 song_artist_sub = re.sub('|\(|\)', '', artist).strip()
                 if song_artist_sub[0].encode().isalpha():
-                    self.list_split['song_artist_sub_eng'] = song_artist_sub.strip()
+                    self.num_sub_eng += 1
+                    self.list_split['song_artist_sub_eng{0}'.format(self.num_sub_eng)] = song_artist_sub.strip()
                 else:
-                    self.list_split['song_artist_sub_kor'] = song_artist_sub.strip()
+                    self.num_sub_kor += 1
+                    self.list_split['song_artist_sub_kor{0}'.format(self.num_sub_kor)] = song_artist_sub.strip()
             else:
                 song_artist_main = re.sub('|\(|\)', '', artist).strip()
                 if song_artist_main[0].encode().isalpha():
-                    self.list_split['song_artist_main_eng'] = song_artist_main
+                    self.num_main_eng += 1
+                    self.list_split['song_artist_main_eng{0}'.format(self.num_main_eng)] = song_artist_main
                 else:
-                    self.list_split['song_artist_main_kor'] = song_artist_main
+                    self.num_main_kor += 1
+                    self.list_split['song_artist_main_kor{0}'.format(self.num_main_kor)] = song_artist_main
 
     def feat_split(self):
-        if self.song_artist_sub_feat[0].encode().isalpha():
-            self.list_split['song_artist_feat_eng'] = self.song_artist_sub_feat
-        else:
-            self.list_split['song_artist_feat_kor'] = self.song_artist_sub_feat
+        self.song_artist_sub_feat = self.song_artist_sub_feat.split(',')
+        for feats in self.song_artist_sub_feat:
+            if feats[0].encode().isalpha():
+                self.num_feat_eng += 1
+                self.list_split['song_artist_feat_eng{0}'.format(self.num_feat_eng)] = feats
+            else:
+                self.num_feat_kor += 1
+                self.list_split['song_artist_feat_kor{0}'.format(self.num_feat_kor)] = feats
 
     def collect_db(self):
         print(self.list_split)
