89a9d5f9fb9841c11046635116d29367be82be15
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-06-04 22:54:52 +0900

    Update youtube_list.py

--- youtube_list.py
+++ youtube_list.py
@@ -5,24 +5,28 @@ import time
 
 class YoutubeList:
     def __init__(self):
+        self.video_num = 3980
         self.read_db()
 
     def read_db(self):
         list_db_music = col1.find({}, {'num': {"$slice": [1, 1]}})
         for x in list_db_music:
             self.num = x['num']
-            if self.num < 0:
+            if self.num < 699:
                 pass
             else:
-                if 'song_artist_main_kor' in x['list_split']:
-                    self.song_artist = x['list_split']['song_artist_main_kor']
+                if 'song_artist_main_kor1' in x['list_split']:
+                    self.song_artist = x['list_split']['song_artist_main_kor1']
                 else:
-                    self.song_artist = x['list_split']['song_artist_main_eng']
+                    self.song_artist = x['list_split']['song_artist_main_eng1']
                 if 'song_title_main_kor' in x['list_split']:
                     self.song_title = x['list_split']['song_title_main_kor']
                 else:
                     self.song_title = x['list_split']['song_title_main_eng']
                 self.pair = self.song_artist + ' ' + self.song_title
+                self.pair = self.pair.replace('%', '%25')
+                self.pair = self.pair.replace('&', '%26')
+
 
                 self.listing_youtube()
 
@@ -45,15 +49,17 @@ class YoutubeList:
         }
 
         for i in search_num_youtube:
+            self.video_num +=1
             self.href = i.attrs['href']
             self.href = "https://youtube.com{0}".format(self.href)
             self.title = i.attrs['aria-label']
             self.title = self.title.split('게시자')[0].strip()
-            print("{0}의 {1}th - {2}".format(self.pair, count, self.title))
+            print("{0}: {1}의 {2}th - {3}".format(self.video_num, self.pair, count, self.title))
+            print('')
             time.sleep(0.2)
 
             video = {
-                'title': self.title, 'link': self.href
+                'title': self.title, 'link': self.href, 'video_num': self.video_num
             }
 
             self.list_video['video{0}'.format(count)] = video
@@ -63,6 +69,10 @@ class YoutubeList:
                 break
 
         print("{0} 비디오 DB 입력 중".format(self.pair))
+
+        if self.video_num % 10 != 0:
+            driver.close()
+            raise IndexError
 
         col2.insert_one(self.list_video).inserted_id
 
