7047fdee5b4d3f29b2d6e0d7a018374a177e8e08
jimp30 <73019663+jimp30@users.noreply.github.com>
2021-06-06 14:15:31 +0900

    Update genie_list.py

--- genie_list.py
+++ genie_list.py
@@ -21,8 +21,8 @@ class GenieList:
             else:
                 self.song_title = x['list_split']['song_title_main_eng']
             self.pair = self.song_artist + ' ' + self.song_title
-            self.pair = self.pair.replace('%', '%25')
-            self.pair = self.pair.replace('&', '%26')
+            self.pair = self.pair.replace('%', '%2525')
+            self.pair = self.pair.replace('&', '%2526')
 
             self.listing_genie()
 
