0f727119500e393f82d3432de9fee8ec3480f682
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-06-11 11:37:09 +0900

    Update daily_to_cow_db.py

--- daily_to_cow_db.py
+++ daily_to_cow_db.py
@@ -8,6 +8,7 @@ class DailyToCowDB:
         self.db_youtube()
         self.db_genie()
         self.db_music_cow()
+        # self.db_back_up()
 
     def db_youtube(self):
         list_db_you_daily = col2.find({}, {'num': {"$slice": [1, 1]}})
@@ -33,11 +34,16 @@ class DailyToCowDB:
             col5.update_one({'num': num}, {'$set': {self.date_today: self.date_data}}, upsert=True)
         col6.delete_many({})
 
+    # def db_back_up(self):
+    #     for i in range(1,7):
+    #         ['col{}'.format(i)].aggregate([{'$match':{}}, {'$out': ['col{}'.format(i)]}])
+
 
 if __name__ == '__main__':
     client = MongoClient('localhost', 27017)
     db1 = client.music_cow
     db2 = client.daily_crawler
+    db3 = client.music_cow_backup
     # daily youtube update
     col1 = db1.daily_youtube
     col2 = db2.daily_youtube
@@ -47,5 +53,13 @@ if __name__ == '__main__':
     # daily youtube update
     col5 = db1.daily_music_cow
     col6 = db2.daily_music_cow
+    # back_up db collection
+    # col_copy1 = db3.comment_youtube
+    # col_copy2 = db3.daily_music_cow
+    # col_copy3 = db3.daily_youtube
+    # col_copy4 = db3.genie_list
+    # col_copy5 = db3.music_list
+    # col_copy6 = db3.music_list_split
+    # col_copy7 = db3.youtube_list
 
     DailyToCowDB()
