a4f7247a597f035064b8d2c6d697624ab2bacdfc
jimp30 <73019663+jimp30@users.noreply.github.com>
2021-07-01 22:08:31 +0900

    Update daily_to_cow_db.py

--- daily_to_cow_db.py
+++ daily_to_cow_db.py
@@ -13,6 +13,9 @@ class DailyToCowDB:
         # self.db_music_cow()
         # self.db_back_up()
         # 쿠글 2팀 작업
+        # 쿠글 2팀 이윤수
+
+
 
     def upload_github(self):
         # github 업로드
@@ -34,44 +37,4 @@ class DailyToCowDB:
         for x in list_db_you_daily:
             video_num = x['video_num']
             self.date_data = x['{0}'.format(self.date_today)]
-            col1.update_one({'video_num': video_num}, {'$set': {self.date_today: self.date_data}}, upsert=True)
-        col2.delete_many({})
-
-    def db_genie(self):
-        list_db_gen_daily = col4.find({}, {'num': {"$slice": [1, 1]}})
-        for x in list_db_gen_daily:
-            link = x['link']
-            self.date_data = x['{0}'.format(self.date_today)]
-            col3.update_one({'link': link}, {'$set': {self.date_today: self.date_data}}, upsert=True)
-        col4.delete_many({})
-
-    def db_music_cow(self):
-        list_db_muc_daily = col6.find({}, {'num': {"$slice": [1, 1]}})
-        for x in list_db_muc_daily:
-            num = x['num']
-            self.date_data = x['{0}'.format(self.date_today)]
-            col5.update_one({'num': num}, {'$set': {self.date_today: self.date_data}}, upsert=True)
-        col6.delete_many({})
-
-    # def db_back_up(self):
-    #     for i in range(1,7):
-    #         ['col{}'.format(i)].aggregate([{'$match':{}}, {'$out': ['col{}'.format(i)]}])
-
-
-if __name__ == '__main__':
-    client = MongoClient('localhost', 27017)
-    db1 = client.music_cow
-    db2 = client.daily_crawler
-    db3 = client.music_cow_backup
-    # daily youtube update
-    col1 = db1.daily_youtube
-    col2 = db2.daily_youtube
-    # daily genie update
-    col3 = db1.daily_genie
-    col4 = db2.daily_genie
-    # daily youtube update
-    col5 = db1.daily_music_cow
-    col6 = db2.daily_music_cow
-    # back_up db collection
-
-    DailyToCowDB()
+            col1.update_
