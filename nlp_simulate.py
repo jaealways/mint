892aa1c833783f248b212f4ad87baaa899aa7a8c
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-08-11 22:41:28 +0900

    Update nlp_simulate.py

--- nlp_simulate.py
+++ nlp_simulate.py
@@ -14,10 +14,9 @@ col1 = db1.article_text
 
 class ArticleNlp:
     def __init__(self):
-        # self.db_read()
+        self.db_read()
         self.tokenization()
         # self.after_token()
-        # self.de_tokenization()
         # self.topic_modeling
 
     def db_read(self):
@@ -30,19 +29,14 @@ class ArticleNlp:
             result = {'num': x['num'], 'song_title': x['song_title'], 'song_artist': x['song_artist'], 'link': x['link'],
                       'article_title': x['article_title'], 'publish': x['publish'], 'text': x['text'], 'date': x['date']}
             df = df.append(result, ignore_index=True)
-            sens = x['text'].split('.')
+            sen = x['text'].replace('.', '')
             nlp_article.append(re.sub(r'[^ ㄱ-ㅣ가-힣A-Za-z]', '', x['article_title']))
-            for sen in sens:
-                if sen == '':
-                    continue
-                nlp_article.append(re.sub(r'[^ ㄱ-ㅣ가-힣A-Za-z]', '', sen))
+            nlp_article.append(re.sub(r'[^ ㄱ-ㅣ가-힣A-Za-z]', '', sen))
             df_temp = pd.DataFrame(nlp_article)
             df_nlp = df_nlp.append(df_temp)
         df_nlp.columns = ['text']
         df.to_pickle('df_article.pkl')
         df_nlp.to_pickle('df_sens_article.pkl')
-        print(df_nlp)
-
 
     def tokenization(self):
         df = pd.read_pickle('df_article.pkl')
@@ -59,18 +53,35 @@ class ArticleNlp:
         for i in df_nlp['tokenized']:
             temp = []
             for j in i[0][0]:
-                temp.append(j[0])
+                if j[0] in ['co', 'kr', 'com']:
+                    continue
+                if j[1] in ['NNG', 'NNP', 'NNB', 'NR', 'NP', 'VV', 'VA', 'VX', 'XR', 'SL']:
+                    temp.append(j[0])
             tokenized_list.append(temp)
-        print(tokenized_list)
-        print('test')
+        df_nlp['after_token'] = tokenized_list
+        index_new = [a for a in range(len(df_nlp))]
+        df_nlp.set_index([index_new])
+        tokenized_list = df_nlp['after_token'].apply(lambda x: [word for word in x if len(word) > 1])
+        index_new = [a for a in range(len(tokenized_list))]
+        tokenized_list.index = index_new
+        print(tokenized_list[:5])
 
-    def de_tokenization(self):
         detokenized_doc = []
-        for i in range(len(text)):
-            t = ' '.join(tokenized_doc[i])
+        for i in range(len(tokenized_list)):
+            t = ' '.join(tokenized_list[i])
             detokenized_doc.append(t)
 
-        text['headline_text'] = detokenized_doc # 다시 text['headline_text']에 재저장
+        df_nlp['tokenized'] = detokenized_doc
+        vectorizer = TfidfVectorizer(max_features=1000, min_df=8)
+        X = vectorizer.fit_transform(df_nlp['tokenized'])
+        X.shape
+
+        lda_model = LatentDirichletAllocation(n_components=5, learning_method='online', random_state=777, max_iter=1)
+        lda_top = lda_model.fit_transform(X)
+        terms = vectorizer.get_feature_names()
+
+        for idx, topic in enumerate(lda_model.components_):
+            print("Topic %d:" % (idx+1), [(terms[i], topic[i].round(2)) for i in topic.argsort()[:-15 - 1:-1]])
 
 
 ArticleNlp()
