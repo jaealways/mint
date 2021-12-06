6bc50e7662dc42158a3f7f8b0aadea5bff3de5c4
jaealways <71856506+jaealways@users.noreply.github.com>
2021-12-06 23:29:57 +0900

    Update mongo_to_sql.py

--- data_transformation/mongo_to_sql.py
+++ data_transformation/mongo_to_sql.py
@@ -2,3 +2,25 @@ from pymongo import MongoClient
 import pymysql
 
 # mongodb 저장 형식을 sql 형식으로 옮기기, 연산처리 빠르게
+
+class MongoToSQL:
+    def __init__(self):
+        self.create_db()
+
+    def create_db(self):
+        with open("../env.json", "r") as env:
+            env_dict = json.load(env)
+        sql_db_pw = env_dict["sql_password"]
+        conn = pymysql.connect(host='127.0.0.1', user='root', password=sql_db_pw, db='mu_tech', charset='utf8')
+
+        try:
+            with conn.cursor() as cursor:
+                sql = 'CREATE DATABASE mu_tech'
+                cursor.execute(sql)
+            conn.commit()
+        finally:
+            conn.close()
+
+        cursor = conn.cursor()
+        conn.commit()
+
