ede48939dc861e159f3b0a06edba47db593d4f3f
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-12-09 23:30:22 +0900

    Update mongo_to_sql.py

--- data_transformation/mongo_to_sql.py
+++ data_transformation/mongo_to_sql.py
@@ -7,18 +7,17 @@ import json
 # mongodb 저장 형식을 sql 형식으로 옮기기, 연산처리 빠르게
 
 class MongoToSQL:
-    def sql_music_list(self):
+    def create_table_music_list(self):
         conn = DbEnv().mongo_connect('music_cow', 'daily_music_cow')
         dict_col = conn.find({}, {'num': 1, '_id': 0})
         list_col = []
 
         for x in dict_col:
-            k = x.keys()
-            v = x[k]
+            v = list(x.values())[0]
             list_col.append(v)
-        # list_col.sort()
+        list_col.sort()
         print(list_col)
 
 
-MongoToSQL().sql_music_list()
+MongoToSQL().create_table_music_list()
 
