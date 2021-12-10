203ba868eb8e2e1e5f2022ef0de71a0f1ee17e1a
LEE JAE-HYEONG (JAY LEE) <jhyj121000@gmail.com>
2021-12-10 23:20:43 +0900

    Update mongo_to_sql.py

--- data_transformation/mongo_to_sql.py
+++ data_transformation/mongo_to_sql.py
@@ -7,8 +7,20 @@ import json
 # mongodb 저장 형식을 sql 형식으로 옮기기, 연산처리 빠르게
 
 class MongoToSQL:
-    def create_table_music_list(self):
-        conn = DbEnv().mongo_connect('music_cow', 'daily_music_cow')
+    def set_df_music_list(self):
+        conn_mongo = DbEnv().connect_mongo('music_cow', 'daily_music_cow')
+        conn_sql, cursor_sql = DbEnv().connect_sql()
+
+        sql_col = """id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        email varchar(255) NOT NULL,
+        password varchar(255) NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8"""
+        last_col = DbEnv().get_last_row(conn_sql, cursor_sql, 'date', sql_col)
+
+        dict_col = conn_mongo.find({'$gte'})
+
+    def update_table_music_list(self):
+        conn = DbEnv().connect_mongo('music_cow', 'daily_music_cow')
         dict_col = conn.find({}, {'num': 1, '_id': 0})
         list_col = []
 
@@ -19,5 +31,11 @@ class MongoToSQL:
         print(list_col)
 
 
-MongoToSQL().create_table_music_list()
+# daily routine
+mongo_sql = MongoToSQL()
+
+DbEnv().create_db('mu_tech')
+
+mongo_sql.set_df_music_list()
+mongo_sql.update_table_music_list()
 
