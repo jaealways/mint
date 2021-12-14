91654aee9e993a6ec4418ce5f7e0735c905bfbe1
jaealways <71856506+jaealways@users.noreply.github.com>
2021-12-14 11:20:03 +0900

    Update mongo_to_sql.py

--- data_transformation/mongo_to_sql.py
+++ data_transformation/mongo_to_sql.py
@@ -1,35 +1,113 @@
 from db_env import DbEnv
 
 from pymongo import MongoClient
+import pandas as pd
 import pymysql
 import json
 
 # mongodb 저장 형식을 sql 형식으로 옮기기, 연산처리 빠르게
 
 class MongoToSQL:
-    def set_df_music_list(self):
-        conn_mongo = DbEnv().connect_mongo('music_cow', 'daily_music_cow')
+    def get_col_daily_music_cow(self):
         conn_sql, cursor_sql = DbEnv().connect_sql()
+        sql_col = """num int(11) NOT NULL,
+        date varchar(255) NOT NULL,
+        price int(11) NOT NULL"""
+        conn_sql, cursor_sql = DbEnv().create_table(conn_sql, cursor_sql, 'daily_music_cow', sql_col)
+        col_list = DbEnv().get_col_list(cursor_sql, 'daily_music_cow')
+        col_list = [item[0] for item in col_list]
+        last_col = DbEnv().get_last_row(cursor_sql, 'daily_music_cow', 'date')
+        print(col_list, last_col)
 
-        #sql 컬럼 어떻게??
-        sql_col = """id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        email varchar(255) NOT NULL,
-        password varchar(255) NOT NULL
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8"""
-        last_col = DbEnv().get_last_row(conn_sql, cursor_sql, 'date', sql_col)
+        return last_col, col_list
 
-        dict_col = conn_mongo.find({'$gte'})
+    def get_col_daily_youtube(self):
+        conn_sql, cursor_sql = DbEnv().connect_sql()
+        sql_col = """song_num int(11) NOT NULL,
+        video_num int(11) NOT NULL,
+        date varchar(255) NOT NULL,
+        viewCount int(11) NOT NULL,
+        likeCount int(11) NOT NULL,
+        dislikeCount int(11) NOT NULL,
+        favoriteCount int(11) NOT NULL,
+        commentCount int(11) NOT NULL"""
+        conn_sql, cursor_sql = DbEnv().create_table(conn_sql, cursor_sql, 'daily_youtube', sql_col)
+        col_list = DbEnv().get_col_list(cursor_sql, 'daily_youtube')
+        col_list = [item[0] for item in col_list]
+        last_col = DbEnv().get_last_row(cursor_sql, 'daily_youtube', 'date')
+        print(col_list, last_col)
 
-    def update_table_music_list(self):
-        conn = DbEnv().connect_mongo('music_cow', 'daily_music_cow')
-        dict_col = conn.find({}, {'num': 1, '_id': 0})
-        list_col = []
+        return last_col, col_list
 
+    def get_col_daily_genie(self):
+        conn_sql, cursor_sql = DbEnv().connect_sql()
+        sql_col = """song_id int(11) NOT NULL,
+        date varchar(255) NOT NULL,
+        total_listener int(11) NOT NULL,
+        total_play int(11) NOT NULL,
+        total_like int(11) NOT NULL"""
+        conn_sql, cursor_sql = DbEnv().create_table(conn_sql, cursor_sql, 'daily_genie', sql_col)
+        col_list = DbEnv().get_col_list(cursor_sql, 'daily_genie')
+        col_list = [item[0] for item in col_list]
+        last_col = DbEnv().get_last_row(cursor_sql, 'daily_genie', 'date')
+        print(col_list, last_col)
+
+        return last_col, col_list
+
+    def update_sql_daily_music_cow(self, col_mongo, table_sql):
+        conn_mongo = DbEnv().connect_mongo('music_cow', col_mongo)
+        conn_sql, cursor_sql = DbEnv().connect_sql()
+        dict_col = conn_mongo.find()
+        list_col = "num, date, price"
         for x in dict_col:
-            v = list(x.values())[0]
-            list_col.append(v)
-        list_col.sort()
-        print(list_col)
+            num = x['num']
+            for index, (key, elem) in enumerate(x.items()):
+                if str(type(elem)) == "<class 'dict'>":
+                    if key > str(last_col):
+                        tuple_data = (num, key, int(elem['price']))
+                        DbEnv.insert_data_to_table(self, conn=conn_sql, cursor=cursor_sql, list_col=list_col,
+                                                   table_sql=table_sql, tuple_data=tuple_data)
+                        print(key, num, int(elem['price']))
+
+    def update_sql_daily_youtube(self, col_mongo, table_sql):
+        conn_mongo = DbEnv().connect_mongo('music_cow', col_mongo)
+        conn_sql, cursor_sql = DbEnv().connect_sql()
+        dict_col = conn_mongo.find()
+        list_col = "song_num, video_num, date, viewCount, likeCount, dislikeCount, favoriteCount, commentCount"
+        for x in dict_col:
+            song_num, video_num = x['num'], x['video_num']
+            for index, (key, elem) in enumerate(x.items()):
+                if str(type(elem)) == "<class 'dict'>":
+                    if key > str(last_col):
+                        for col in col_list:
+                            if col not in elem.keys():
+                                elem[col] = 0
+                        tuple_data = (song_num, video_num, key, int(elem['viewCount']), int(elem['likeCount']),
+                                      int(elem['dislikeCount']), int(elem['favoriteCount']), int(elem['commentCount']))
+                        DbEnv.insert_data_to_table(self, conn=conn_sql, cursor=cursor_sql, list_col=list_col,
+                                                   table_sql=table_sql, tuple_data=tuple_data)
+                        print(song_num, video_num, key, int(elem['viewCount']), int(elem['likeCount']),
+                                      int(elem['dislikeCount']), int(elem['favoriteCount']), int(elem['commentCount']))
+
+    def update_sql_daily_genie(self, col_mongo, table_sql, col_list):
+        conn_mongo = DbEnv().connect_mongo('music_cow', col_mongo)
+        conn_sql, cursor_sql = DbEnv().connect_sql()
+        dict_col = conn_mongo.find()
+        list_col = "song_id, date, total_listener, total_play, total_like"
+        for x in dict_col:
+            song_id = x['link'].split('xgnm=')[1]
+            for index, (key, elem) in enumerate(x.items()):
+                # 각 변수에 맞게 할당하기
+                if str(type(elem)) == "<class 'dict'>":
+                    if key > str(last_col):
+                        for col in col_list:
+                            if col not in elem.keys():
+                                elem[col] = 0
+                        tuple_data = (song_id, key, int(elem['total_listener']), int(elem['total_play']), int(elem['like']))
+                        DbEnv.insert_data_to_table(self, conn=conn_sql, cursor=cursor_sql, list_col=list_col,
+                                                   table_sql=table_sql, tuple_data=tuple_data)
+                        print(song_id, key, int(elem['total_listener']), int(elem['total_play']), int(elem['like']))
+
 
 
 # daily routine
@@ -37,6 +115,12 @@ mongo_sql = MongoToSQL()
 
 DbEnv().create_db('mu_tech')
 
-mongo_sql.set_df_music_list()
-mongo_sql.update_table_music_list()
+# last_col, col_list = mongo_sql.get_col_daily_music_cow()
+# mongo_sql.update_sql_daily_music_cow('daily_music_cow', 'daily_music_cow')
+
+# last_col, col_list = mongo_sql.get_col_daily_youtube()
+# mongo_sql.update_sql_daily_youtube('daily_youtube', 'daily_youtube', col_list)
+
+last_col, col_list = mongo_sql.get_col_daily_genie()
+mongo_sql.update_sql_daily_genie('daily_genie', 'daily_genie', col_list)
 
