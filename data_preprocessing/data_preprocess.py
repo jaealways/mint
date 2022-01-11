564a3951276a6bfbf481a3c9eb1c431a6f17d0a4
jaealways <71856506+jaealways@users.noreply.github.com>
2022-01-11 23:43:43 +0900

    Update data_preprocess.py

--- data_preprocessing/data_preprocess.py
+++ data_preprocessing/data_preprocess.py
@@ -10,41 +10,27 @@ from tensorflow import keras
 
 
 class DataPreprocess:
-    def make_df_nan_same(self, df_a, df_b, col):
-        """df_a의 col 값을 기준으로, df_b 통합하여 결측치 제거"""
-        df_a = df_a.reset_index()
-        df_b = df_b.reset_index()
-
-        df_a = pd.merge(df_a, df_b, how='inner', on=col)
-        df_a = df_a.drop_duplicates()
-        df_a = df_a.set_index(col)
-
-        df_a_droped = df_a.dropna(axis=0)
-        df_a_droped = df_a_droped.sort_index(ascending=True)
-        list_a_droped = list(df_a_droped.columns)
-
-        df_b_droped = pd.DataFrame(df_a_droped[df_a_droped.columns[-(len(df_b.columns)-1)]])
-        df_a_droped = pd.DataFrame(df_a_droped.drop(df_a_droped.columns[-(len(df_b.columns)-1)], axis='columns'))
-
-        return df_a_droped, df_b_droped, list_a_droped
-
     def scale_df(self, df, method='Standard'):
-        list_df_droped = df.columns
+        list_df = df.columns
+        df_t = np.transpose(df)
         if method == 'MinMax':
-            array_scaled = MinMaxScaler().fit_transform(df)
+            array_scaled = MinMaxScaler(feature_range=(0.01, 1)).fit_transform(df_t)
         else:
-            array_scaled = StandardScaler().fit_transform(df)
-        df_scaled = pd.DataFrame(array_scaled)
-        df_scaled.columns = list_df_droped
+            array_scaled = StandardScaler().fit_transform(df_t)
+        df_scaled_t = pd.DataFrame(array_scaled)
+        df_scaled = np.transpose(df_scaled_t)
+        df_scaled.columns = list_df
         df_scaled.index = df.index
 
         return df_scaled
 
     def scale_array(self, array, method='Standard'):
+        array_t = np.transpose(array)
         if method == 'MinMax':
-            array_scaled = MinMaxScaler(feature_range=(0.01, 1),).fit_transform(array)
+            array_scaled_t = MinMaxScaler(feature_range=(0.01, 1)).fit_transform(array_t)
         else:
-            array_scaled = StandardScaler().fit_transform(array)
+            array_scaled_t = StandardScaler().fit_transform(array_t)
+        array_scaled = np.transpose(array_scaled_t)
 
         return array_scaled
 
@@ -57,23 +43,22 @@ class DataPreprocess:
         sae: Stacked Auto-encoder
         """
         array_time = df_time.to_numpy()
-        array_time_t = np.transpose(array_time)
 
         if method == "exp":
-            list_time_smooth_t = [SimpleExpSmoothing(x).fit(smoothing_level=level).fittedvalues for x in array_time_t]
+            list_time_smooth = [SimpleExpSmoothing(x).fit(smoothing_level=level).fittedvalues for x in array_time]
         elif method == "holt":
-            list_time_smooth_t = [Holt(x).fit(smoothing_level=level).fittedvalues for x in array_time_t]
+            list_time_smooth = [Holt(x).fit(smoothing_level=level).fittedvalues for x in array_time]
         elif method == "wave":
-            list_time_smooth_t = []
-            for x in array_time_t:
+            list_time_smooth = []
+            for x in array_time:
                 coeff = pywt.wavedec(x, "haar", mode='periodization', level=level, axis=0)
                 sigma = mad(coeff[-level])
                 uthresh = sigma * np.sqrt(2 * np.log(len(x)))
                 coeff[1:] = (pywt.threshold(i, value=uthresh, mode="hard") for i in coeff[1:])
-                list_time_smooth_t.append(pywt.waverec(coeff, "haar", mode='periodization', axis=0))
+                list_time_smooth.append(pywt.waverec(coeff, "haar", mode='periodization', axis=0))
         elif method == "sae":
-            list_time_smooth_t = []
-            for x in array_time_t:
+            list_time_smooth = []
+            for x in array_time:
                 input_dim = x.shape[0]
                 hidden_dim = 10
 
@@ -100,10 +85,9 @@ class DataPreprocess:
 
                 autoencoder.fit(x[0], x[0], epochs=10, verbose=2)
                 autoencoded_data = autoencoder.predict(x[0])
-                list_time_smooth_t.append(autoencoded_data)
+                list_time_smooth.append(autoencoded_data)
 
-        array_time_smooth = np.array(list_time_smooth_t).transpose()
-        df_time_smooth = pd.DataFrame(array_time_smooth)
+        df_time_smooth = pd.DataFrame(np.array(list_time_smooth))
         df_time_smooth.columns = df_time.columns
         df_time_smooth.index = df_time.index
 
