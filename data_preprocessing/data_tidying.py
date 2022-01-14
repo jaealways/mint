943f75ed1a42ad6077adddc005f4b6366070ace6
jaealways <71856506+jaealways@users.noreply.github.com>
2022-01-14 23:42:20 +0900

    Update data_tidying.py

--- data_preprocessing/data_tidying.py
+++ data_preprocessing/data_tidying.py
@@ -21,7 +21,6 @@ class DataTidying:
         for num in tqdm(list_num):
             sql = "SELECT DISTINCT date, price_close FROM daily_music_cow WHERE (num = '%s') AND (date BETWEEN '%s' AND '%s') ORDER BY date" % (num, '20'+str_date, '20'+end_date)
             df_temp = db(cursor, sql).dataframe
-            # df_temp.rename(columns={'DISTINCTdate': 'date'}, inplace=True)
             df_temp = df_temp.set_index('date')
             df_temp.columns = ["%d" % num]
 
@@ -35,7 +34,6 @@ class DataTidying:
     def get_df_mcpi(self, cursor, str_date='17-01-01', end_date='23-12-31'):
         sql = "SELECT DISTINCT date, price FROM daily_mcpi WHERE (date BETWEEN '%s' AND '%s') ORDER BY date" % ('20'+str_date, '20'+end_date)
         df_mcpi = db(cursor, sql).dataframe
-        # df_mcpi.rename(columns={'DISTINCTdate': 'date'}, inplace=True)
 
         df_mcpi = df_mcpi.set_index('date')
         df_mcpi.columns = [0]
@@ -44,4 +42,32 @@ class DataTidying:
         df_mcpi.to_pickle("../storage/df_raw_data/df_mcpi_%s_%s.pkl" % (str_date, end_date))
 
         return df_mcpi
+
+    def get_df_song_volume(self, cursor, str_date='17-01-01', end_date='23-12-31'):
+        df_song_volume = pd.DataFrame()
+        df_mcpi = pd.read_pickle("../storage/df_raw_data/df_mcpi_17-01-01_23-12-31.pkl")
+        list_mcpi_date = df_mcpi.loc[:, '20' + str_date: '20' + end_date].columns.tolist()
+        for date in tqdm(list_mcpi_date):
+            sql = "SELECT DISTINCT num, volume FROM daily_music_cow WHERE date='%s' ORDER BY num" % (date)
+            df_temp = db(cursor, sql).dataframe
+            df_temp = df_temp.set_index('num')
+            df_temp.columns = ["%s" % date]
+
+            df_song_volume = pd.concat([df_song_volume, df_temp], axis=1)
+
+        df_song_volume.to_pickle("../storage/df_raw_data/df_song_volume_%s_%s.pkl" % (str_date, end_date))
+
+        return df_song_volume
+
+    def get_df_fng_index(self, str_date='17-01-01', end_date='23-12-31'):
+        df_mcpi = pd.read_pickle("../storage/df_raw_data/df_mcpi_17-01-01_23-12-31.pkl")
+        df_song_volume = pd.read_pickle("../storage/df_raw_data/df_song_volume_17-01-01_23-12-31.pkl")
+
+        df_mcpi_volume = np.transpose(pd.DataFrame(df_song_volume.sum()))
+        df_fng_index = pd.concat([df_mcpi, df_mcpi_volume], axis=0)
+
+        df_mcpi_volume.to_pickle("../storage/df_raw_data/df_mcpi_volume_%s_%s.pkl" % (str_date, end_date))
+        df_fng_index.to_pickle("../storage/df_raw_data/df_fng_index_%s_%s.pkl" % (str_date, end_date))
+
+        return df_mcpi_volume, df_fng_index
 
