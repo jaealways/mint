d3ce355cc760efae7b3cb28d3bcc05b588e2e699
nina8912205 <71542788+nina8912205@users.noreply.github.com>
2022-02-17 15:29:49 +0900

    Update mainCrawler.py

--- crawlers/mainCrawler.py
+++ crawlers/mainCrawler.py
@@ -1,8 +1,18 @@
+# << 메인 크롤링 코드 >>
+# 작성자 : 정예원
+
+# 코드 개선사항
+# 크롤링 1 종료후
+# 크롤링 2 크롤링 3 크롤링 4 thread 나눠서 시간 단축
+
+# modules
 import musicCowCrawler
-from pymongo import MongoClient
 import copyrightPriceCrawler
 
-# == 몽고디비 ==
+from pymongo import MongoClient
+
+
+ # == 몽고디비 ==
 client = MongoClient('localhost', 27017)
 db1 = client.music_cow
 db2 = client.article
@@ -10,7 +20,7 @@ db2 = client.article
 # music cow
 col1 = db1.musicCowData
 col2 = db1.mcpi
-col3 = db1.copyright
+col3 = db1.copyright_price
 
 # article
 col4 = db2.articleInfo
@@ -22,24 +32,16 @@ musicCowArtistListCurrent = col1.find({}, {'song_artist': {"$slice": [1, 1]}})  
 # == 크롤링 ==
 
 # 1. 뮤직카우 데이터 크롤링
-print("<< 뮤직카우 데이터 크롤링을 시작합니다 >>")
-musicCowData = musicCowCrawler.MusicCowCrawler(col1, musicCowSongNumListCurrent, musicCowArtistListCurrent)
-musicCowData.songCrawler()      # 뮤직카우 디비에 있는 기존 곡들 크롤링
-musicCowData.songCrawlerNew()   # 뮤직카우에 새로 등록된 곡들 크롤링
-print("뮤직카우에 새로 등록된 가수 명단입니다")
-print(musicCowData.newArtistList)
+# print("<< 뮤직카우 데이터 크롤링을 시작합니다 >>")
+# musicCowData = musicCowCrawler.MusicCowCrawler(col1, col3, musicCowSongNumListCurrent, musicCowArtistListCurrent)
+# musicCowData.songCrawler()      # 뮤직카우 디비에 있는 기존 곡들 크롤링
+# musicCowData.songCrawlerNew()   # 뮤직카우에 새로 등록된 곡들 크롤링
+# print("뮤직카우에 새로 등록된 가수 명단입니다")
+# print(musicCowData.newArtistList)
 
 # input("")
-#
+
 # # 2. 저작권료 크롤링
-# print("<< 저작권료 크롤링을 시작합니다 >> ")
-# copyrightPriceCrawler.CopyrightPriceCrawler()
-#
-# # 3. mcpi 크롤링
-# print("<< mcpi 크롤링을 시작합니다 >> ")
-#
-#
-# # 4. article + article_text 크롤링
-# print("<< article 크롤링을 시작합니다 >> ")
-# print("<< article_text 크롤링을 시작합니다 >> ")
-#
+print("<< 저작권료 크롤링을 시작합니다 >> ")
+copyrightPriceData = copyrightPriceCrawler.CopyrightPriceCrawler(col1, col3)
+copyrightPriceData.copyrightPrice()
