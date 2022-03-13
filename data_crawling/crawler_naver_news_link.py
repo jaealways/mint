71e5e18f6877820e3cec840869daefab648fddaf
jaealways <71856506+jaealways@users.noreply.github.com>
2022-03-13 23:42:40 +0900

    Update crawler_naver_news_link.py

--- data_crawling/crawler_naver_news_link.py
+++ data_crawling/crawler_naver_news_link.py
@@ -8,13 +8,13 @@ from data_crawling import artist_for_nlp
 
 
 class daily_Naver:
-    def __init__(self, col, ArtistList):
+    def __init__(self, col, ArtistList, NewsArtistListLong, NewsDateListLong):
         self.article_num = 0
-        self.read_db(ArtistList)
         self.count = 0
         self.col = col
+        self.read_db(ArtistList, NewsArtistListLong, NewsDateListLong)
 
-    def read_db(self, ArtistList):
+    def read_db(self, ArtistList, NewsArtistListLong, NewsDateListLong):
         self.link_num = 0
         artist_list = ArtistList
 
@@ -23,22 +23,28 @@ class daily_Naver:
             # self.num = 30
             self.keyword = artist
             print('{0}검색 시작'.format(artist))
-            self.listing_article()
+            self.listing_article(NewsArtistListLong, NewsDateListLong)
 
-    def listing_article(self):
+    def listing_article(self, NewsArtistListLong, NewsDateListLong):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
-        today = datetime.date.today() - datetime.timedelta(days=1)
+        today = datetime.date.today()
         # today = datetime.date(2020, 8, 9)
         try:
-            mongoDate = list(map(lambda x: x['article_title'], list(self.col.find({'artist': self.keyword}))))
-            liststop = list(set(mongoDate))
+            # mongoDate = list(map(lambda x: x['date'], list(self.col.find({'artist': self.keyword}))))
+            idx_list = list(filter(lambda x: NewsArtistListLong[x] == self.keyword, range(len(NewsArtistListLong))))
+            list_date = [NewsDateListLong[x] for x in idx_list]
+            liststop = list(set(list_date))
             liststop.sort()
             stop = liststop[-1]
         except:
             stop = '2019-01-01'
 
         while 1:
+            today += datetime.timedelta(-1)
+            if today.strftime('%Y-%m-%d') <= stop:
+                break
+
             self.page = 'https://search.naver.com/search.naver?where=news&sm=tab_pge&query={0}&sort=1&photo=0&field=0&pd=3&ds={1}&de={1}&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:dd,p:from20210603to20210713,a:all&start='.format(
                 self.keyword, today.strftime('%Y.%m.%d'))
             #             print(self.page)
@@ -61,7 +67,7 @@ class daily_Naver:
                             title = con.select_one('a.news_tit')
                             link = temp.attrs['href']
                             print('{0} 검색 결과 ----------------------------------------------------'.format(self.keyword))
-                            print('{0}번째 기사'.format(self.link_num))
+                            print('날짜 {0}'.format(today.strftime('%Y-%m-%d')))
                             print('네이버 검색 페이지: ', article_link)
                             print('기사 링크: ', link)
                             self.articles = {
@@ -125,9 +131,5 @@ class daily_Naver:
                                 pass
                         self.link_num += 1
 
-
                 else:
                     break
-            today += datetime.timedelta(-1)
-            if today.strftime('%Y-%m-%d') == stop:
-                break
