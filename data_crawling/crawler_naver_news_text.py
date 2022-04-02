77e21aeb905fcf7682e9357304ae24f8a8a66da8
jaealways <71856506+jaealways@users.noreply.github.com>
2022-04-02 22:23:51 +0900

    Update crawler_naver_news_text.py

--- data_crawling/crawler_naver_news_text.py
+++ data_crawling/crawler_naver_news_text.py
@@ -9,23 +9,22 @@ from multiprocessing import Pool
 
 class update_article_info:
     def __init__(self, dateToday, col5, ArtistList):
-        self.make_list_text(dateToday, col5, ArtistList)
-
-    def make_list_text(self, dateToday, col5, ArtistList):
-        headers = {
+        self.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36"}
-        for db_artist in ArtistList:
-            articles = list(col5.find({'artist': db_artist, 'date': {'$gte': "2021-12-01"}, 'text': {'$exists': False}}))
-            [lambda x: self.text_crawler(x, col5, headers, dateToday), articles]
+        [self.make_link_list(dateToday, col5, artist) for artist in ArtistList]
 
-    def text_crawler(self, doc, col, headers, dateToday):
+
+    def make_link_list(self, dateToday, col5, artist):
+        articles = list(col5.find({'artist': artist, 'text': {'$exists': False}}).distinct("link"))
+        print('artist: {0}, {1}개'.format(artist, len(articles)))
+        [self.text_crawler(doc, col5, self.headers, dateToday) for doc in articles]
+
+    def text_crawler(self, doc, col, dateToday):
         link = doc['link']
         #             print(doc['link_num'])
-        if doc['link_num'] % 100 == 0:
-            print(doc['link_num'])
 
         try:
-            req = requests.get(link, headers=headers)
+            req = requests.get(link, headers=self.headers)
             soup = BeautifulSoup(req.text, 'html.parser')
             try:
                 if '/news.naver' in link:
@@ -67,10 +66,27 @@ class update_article_info:
                 print('오류난 링크 :', link)
                 error.append(link)
                 text = ' '
-            col.update_one({'_id': doc['_id']}, {'$set': {'text': text, 'date_crawler': dateToday.strftime('%Y-%m-%d')}}, upsert=True)
+            col.update_one({'link': doc['link']}, {'$set': {'text': text, 'date_crawler': dateToday.strftime('%Y-%m-%d')}}, upsert=True)
 
         except:
             pass
 
 
 error = []
+
+
+# from multiprocessing import Process, Pool
+#
+#
+# client = MongoClient('localhost', 27017)
+# db1 = client.music_cow
+# db2 = client.article
+# col5 = db2.article_info
+# ArtistList = []
+# dateToday = datetime.datetime.today()
+# update_article_info(dateToday, col5, ArtistList)
+
+# p1 = Process(target=update_article_info(dateToday, col5, ArtistList))
+# p1.start()
+# p2 = Process(target=update_article_info(dateToday, col5, ArtistList))
+# p2.start()
