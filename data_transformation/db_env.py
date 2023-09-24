d0df0ae82eab56c2977f7d51b31b65b8f7a473c5
jaealways <71856506+jaealways@users.noreply.github.com>
2023-09-24 12:08:56 +0900

    Update db_env.py

--- data_transformation/db_env.py
+++ data_transformation/db_env.py
@@ -20,7 +20,6 @@ class DbEnv:
                 env_dict = json.load(env)
         sql_db_pw = env_dict["sql_password"]
         conn = pymysql.connect(host='127.0.0.1', user='root', password=sql_db_pw, db='mu_tech', charset='utf8mb4')
-        # conn = pymysql.connect(host='kuggle.csr6lxslx7sg.ap-northeast-2.rds.amazonaws.com', user='kuggle', password='musiccow', db='mu_tech', charset='utf8mb4')
 
         cursor = conn.cursor()
         conn.commit()
